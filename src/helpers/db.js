eafed4c83db7b521a386298409ef5eb675c27763
katiamiyu <katiamiyu@gmail.com>
2020-02-17 14:51:43 +0100

    feat(run todo test):
    run test on all todo endpoints
    check for parameters
    outline all test reports
    
    [Finishes A0000112]

--- src/helpers/db.js
+++ src/helpers/db.js
@@ -1,32 +1,31 @@
-/** 
+/**
  *  @fileoverview file to query database
  *  @class QueryController
  *  @export QueryController
  */
 class QueryController {
-  /** 
-   *  @param {object} response 
-   *  @param {string} status 
-   *  @param {string} message 
-   *  @param {object} data 
+  /**
+   *  @param {object} response
+   *  @param {string} status
+   *  @param {string} message
+   *  @param {object} data
    *  @returns json
    */
   static getSuccess(response, status, message, data) {
     // return a success message
-    return response.status(status).json({ status, message, data, })
-  };
+    return response.status(status).json({ status, message, data });
+  }
 
   /**
-   * @param {object} response 
-   * @param {string} status 
-   * @param {string} error 
+   * @param {object} response
+   * @param {string} status
+   * @param {string} error
    * @returns json
    */
   static notFoundError(response, status, error) {
     // return not found message
-    return response.status(status).json({ status, error })
+    return response.status(status).json({ status, error });
   }
-
 }
 
 export default QueryController;
